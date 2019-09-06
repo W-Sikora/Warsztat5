cd46c317abd9bd888b5ecea72a31620e09a3d097
Małgorzata Śliwińska-Łyskawa <sliwinska.malgorzata@gmail.com>
2019-09-06 15:02:19 +0200

    Done

--- src/main/java/pl/coderslab/app/AppConfig.java
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -2,6 +2,7 @@ package pl.coderslab.app;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -9,5 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @ComponentScan(basePackages = "pl.coderslab")
 public class AppConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
 
